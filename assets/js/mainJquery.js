0803128c6e9f356293cf5056cb22c0ed8acdf459
mahboob543 <mahboob6103@gmail.com>
2022-02-18 06:43:23 +0330

    send

--- assets/js/mainJquery.js
+++ assets/js/mainJquery.js
@@ -1,16 +1,16 @@
 $(document).ready(function () {
 
-    $(".hamburger").click(function () {
-      $(this).toggleClass("is-active");
-    });
+  $(".hamburger").click(function () {
+    $(this).toggleClass("is-active");
+  });
 
-    $('.three').click(function () {
-      if ($('.three').hasClass("active")) {
-        closeMenu()
-      } else {
-        openMenu()
-      }
-    
+  $('.three').click(function () {
+    if ($('.three').hasClass("active")) {
+      closeMenu()
+    } else {
+      openMenu()
+    }
+
 
 
     function openMenu() {
@@ -18,68 +18,79 @@ $(document).ready(function () {
       $('.drawer-menu').animate(
         {
           width: "20vw",
-      height: "100vh",
+          height: "100vh",
         }, 'slow')
 
       $(".three").animate(
         {
-          marginRight: "0vw",
-          
+          marginRight: "10vw",
+
         }, 'slow')
-            $(".first-logo").css({
+      $(".shift").animate(
+        {
+          marginRight: "21vw",
+
+        }, 'slow')
+
+      $(".first-logo").css({
         display: "none"
 
       })
       $(".extra").text('menu');
-      
+
     }
-      
-  function closeMenu() {
-    $('.three').removeClass("active")
-    $('.drawer-menu').animate(
-      {
-        width: "0",
-        height: "100vh"
-      }, 100)
-    $(".first-logo").css({
-      display: "flex"
 
-    })
-    $(".three").animate(
-      {
-        marginRight: '0vw'
-      }, 'slow')
+    function closeMenu() {
+      $('.three').removeClass("active")
+      $('.drawer-menu').animate(
+        {
+          width: "0",
+          height: "100vh"
+        }, 100)
+      $(".first-logo").css({
+        display: "flex"
+
+      })
+      $(".three").animate(
+        {
+          marginRight: '0vw'
+        }, 'slow')
+      $(".shift").animate(
+        {
+          marginRight: "0vw",
+
+        }, 'slow')
       $(".extra").text('Extra');
-  }
-})
+    }
+  })
 
-// $(window).triggre("resize",function(){
-//   if($(window).width()<=766){
-//     //console.log($(window).width())
-//     $('.just-padding').before(`
-//     <a class="linkTransition">Home</a>
-//     <a class="linkTransition"> rooms</a>
-//     <a class="linkTransition">location</a>
-//     <a class="linkTransition">activities</a>
-//     <a class="linkTransition" >restaurant</a>
-//     <a class="linkTransition">spa</a>
-//     <a class="linkTransition" >contact</a>
-//     <a class="linkTransition">more 
-//     </a>
-//   `);
-//   $('.navbar-ul').css({display:'none'})
+  // $(window).triggre("resize",function(){
+  //   if($(window).width()<=766){
+  //     //console.log($(window).width())
+  //     $('.just-padding').before(`
+  //     <a class="linkTransition">Home</a>
+  //     <a class="linkTransition"> rooms</a>
+  //     <a class="linkTransition">location</a>
+  //     <a class="linkTransition">activities</a>
+  //     <a class="linkTransition" >restaurant</a>
+  //     <a class="linkTransition">spa</a>
+  //     <a class="linkTransition" >contact</a>
+  //     <a class="linkTransition">more 
+  //     </a>
+  //   `);
+  //   $('.navbar-ul').css({display:'none'})
 
-// }else{
-//   $('.navbar-ul').css({display:'flex'})
-// }
-// })
+  // }else{
+  //   $('.navbar-ul').css({display:'flex'})
+  // }
+  // })
 
 
-// submenu
+  // submenu
   $(function () {
     $(".main li").hover(
       function () {
-        $('>ul.subMenu:not(:animated)', this).slideDown(500);
+        $('>ul.subMenu', this).slideDown(500);
       },
       function () {
         $('>ul.subMenu', this).slideUp(300)
@@ -87,7 +98,7 @@ $(document).ready(function () {
       })
 
   })
- 
+
   //make carousel
   $(".owl-carousel").owlCarousel({
     items: 1,
@@ -114,7 +125,7 @@ $(document).ready(function () {
       .addClass("animated fadeInUp");
   });
 
-// make for request button
+  // make for request button
   $(".request").click(function () {
 
     let checkInDate = new Date($("#checkInDate").val()).getTime();
@@ -159,38 +170,38 @@ $(document).ready(function () {
 
 
   let currentSlide = 0,
-  nav= $(".CarouselNav li");
+    nav = $(".CarouselNav li");
 
-function setSlide(index) {
- nav.removeClass("selectedCarouselNav");
- nav.eq(index).addClass("selectedCarouselNav");
-  $(".threeSlides").css("display", "none");
-  $(".threeSlides").eq(index).css("display", "flex");
-}
-
-$(".CarouselNav li").click(function () {
-  let index = $(".CarouselNav li").index($(this));
-  currentSlide = index;
-  setSlide(currentSlide);
-});
-function slide() {
-  if (currentSlide <nav.length - 1) {
-    currentSlide++;
-    setSlide(currentSlide);
-  } else {
-    currentSlide = 0;
-    setSlide(currentSlide);
+  function setSlide(index) {
+    nav.removeClass("selectedCarouselNav");
+    nav.eq(index).addClass("selectedCarouselNav");
+    $(".threeSlides").css("display", "none");
+    $(".threeSlides").eq(index).css("display", "flex");
   }
-}
 
-let interval = setInterval(slide, 3000);  
+  $(".CarouselNav li").click(function () {
+    let index = $(".CarouselNav li").index($(this));
+    currentSlide = index;
+    setSlide(currentSlide);
+  });
+  function slide() {
+    if (currentSlide < nav.length - 1) {
+      currentSlide++;
+      setSlide(currentSlide);
+    } else {
+      currentSlide = 0;
+      setSlide(currentSlide);
+    }
+  }
 
-});   
+  let interval = setInterval(slide, 3000);
+
+});
 
 
- 
 
 
 
 
-  
+
+
